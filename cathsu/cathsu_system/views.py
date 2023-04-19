79327c23da2653da05c41ad373fbdf59a1c1c080
alphaDinar <yanksone18@yahoo.com>
2023-04-19 08:18:41 +0000

    change

--- cathsu/cathsu_system/views.py
+++ cathsu/cathsu_system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Member
 
 # Create your views here.
@@ -6,6 +6,26 @@ def login(request):
     return render(request, 'login.html' )
 
 def add_member(request):
+    if request.method == 'POST':
+        member = Member()
+        member.pro_pic = request.FILES.get('pro_pic')
+        member.first_name = request.POST.get('first_name')
+        member.last_name = request.POST.get('last_name')
+        member.dob = request.POST.get('dob')
+        member.gender = request.POST.get('gender')
+        member.marital_status = request.POST.get('marital_status')
+        member.home_address = request.POST.get('home_address')
+        member.res_address = request.POST.get('res_address')
+        member.contact = request.POST.get('contact')
+        member.email = request.POST.get('email')
+        member.cur_level = request.POST.get('cur_level')
+        member.institution = request.POST.get('institution')
+        member.baptism = request.POST.get('baptism')
+        member.confirmation = request.POST.get('confirmation')
+        member.position = request.POST.get('position')
+        member.year = request.POST.get('year')
+        member.save()
+        return redirect('all_members')
     return render(request, 'add_member.html' )
 
 def all_members(request):
@@ -35,11 +55,5 @@ def dashboard(request):
 
 
 def test(request):
-    if request.method == 'POST':
-        member = Member()
-        member.first_name = request.POST.get('form_first')
-        member.last_name = request.POST.get('form_last')
-        # print(request.FILES.get('form_picture'))
-        member.picture = request.FILES.get('form_picture')
-        member.save()
+   
     return render(request, 'test.html')
